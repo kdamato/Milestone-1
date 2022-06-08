a01237e0a544e05fb12b4636d21f7e32f74d9f79
Kyle DAmato <kyledamato4@gmail.com>
2022-06-08 17:56:00 -0400

    getting questions set up to be linked to boxes

---
+++ Milestone-1/boardQuestions.js
@@ -0,0 +1,82 @@
+let  = [
+    {
+      text: `Who?
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "C"
+    },
+    {
+      text: `What?
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "D"
+    },
+    {
+      text: `Who?
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "B"
+    },
+    {
+      text: `Who?;
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "D"
+    },
+    {
+      text: `Which?
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "C"
+    },
+    {
+      text: `At?
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "A"
+    },
+    {
+      text: `Who?
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "C"
+    },
+    {
+      text: `How?
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "B"
+    },
+    {
+      text: `What?
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "D"
+    },
+    {
+      text: `What?
+        A. 
+        B. 
+        C. 
+        D. `,
+      correctAnswer: "C"
+    }
+  ]
