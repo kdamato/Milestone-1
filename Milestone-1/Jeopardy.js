364ac7aab86385f9845e19ae8790f8ae247a675e
Kyle DAmato <kyledamato4@gmail.com>
2022-06-13 19:53:34 -0400

    created functioning score tracker

--- Milestone-1/Jeopardy.js
+++ Milestone-1/Jeopardy.js
@@ -6,14 +6,274 @@ const category5 = 'category5'
 
 var userScore = 0;
 
+const levels = ['500', '400', '300', '200', '100']
+
+const questions = {
+    level500: [
+        {
+        text: `This movie depicts a jewish father hiding the horrors of the holocaust from his son by turning their time at concentration camps into a game.
+        A. Life is Beautiful
+        B. Schindler's list
+        C. The Pianist
+        D. The Boy in the Striped Pajamas`, 
+        category: category1,
+        correctAnswer: 'A',
+        score: 500,
+        },
+        {
+            text: `This movie is about a man who learns the men in his family can travel beginning on their 21st birthday.
+            A. About time
+            B. In time
+            C. The Adam Project
+            D. The Tomorrow War`,
+            category: category2,
+            correctAnswer: 'A',
+            score: 400,
+        },
+        {
+            text: `A christmas rom-com about a father meeting his rich, future son-in-law for the first time. Little does the father know, his daughters fiancee is his biggest nightmare and he does not approve.
+            A. Why Him?
+            B. Just Friends
+            C. Four Christmases
+            D. Holidate`,
+            correctAnswer: 'A',
+            category: category3,
+            correctAnswer: 'C',
+            score: 300,
+        },
+        {
+            text: `Not your typical superhero movie of a red suited vigilante?
+            A. Deadpool
+            B. Green Lantern
+            C. Thor
+            D. Spider-Man`,
+            category: category4,
+            correctAnswer: 'A',
+            score: 200,
+        },
+        {
+            text: `What childrens movie stars the greek demi-god, the son of Zeus?
+            A. Hercules
+            B. Toy Story
+            C. A Bug's Life
+            D. The Lion King`,
+            category: category5,
+            correctAnswer: 'C',
+            score: 100,
+        }
+    ],
+    level400: [
+        {
+            text: `Who voiced Stuart Little in the 1999 family-film "Stuart Little?
+            A. Michael J. Fox
+            B. Mark Hamill
+            C. Robin Williams
+            D. Jim Carrey`,
+            category: category1,
+            correctAnswer: 'A',
+            score: 500,
+        },
+        {
+            text: `Who played Mrs. Doubtfire in the movie "Mrs. Doubtfire"?
+            A. Robin Williams
+            B. Martin Lawrence
+            C. Will Ferrell
+            D. Eddie Murphy`,
+            category: category2,
+            correctAnswer: 'A',
+            score: 400,
+        },
+        {
+            text: `Who plays Rasputia in Norbit?
+            A. Robin Williams
+            B. Martin Lawrence
+            C. Eddie Murphy
+            D. Will Ferrell`,
+            category: category3,
+            correctAnswer: 'C',
+            score: 300,
+        },
+        {
+            text: `Which voices Darth Vader?
+            A. James Earl Jones
+            B. Hayden Christensen
+            C. David Prowse
+            D. Spencer Wilding`,
+            category: category4,
+            correctAnswer: 'A',
+            score: 200,
+        },
+        {
+            text: `Who plays Jack Sparrow in the Pirates of the Carribean franchise?
+            A. Johnny Depp
+            B. Hugh Jackman
+            C. Matthew McConaughey
+            D. Jim Carrey`,
+            category: category5,
+            correctAnswer: 'C',
+            score: 100,
+        }
+    ],
+    level300: [  {
+        text: `What movie won the 2003 best visual effects Academy Award?
+            A. Master and Commander: The Far Side of the World
+            B. Pirates of the Caribbean: The Curse of the Black Pearl
+            C. The Lord of the Rings: The Return of the King
+            D. King Kong`,
+        category: category1,
+        correctAnswer: 'A',
+        score: 500,
+    },
+    {
+        text: `what is the color of the sky?
+        A. blue
+        B. red
+        C. green
+        D. yellow`,
+        category: category2,
+        correctAnswer: 'A',
+        score: 400,
+    },
+    {
+        text: `what is the color of the sky?
+        A. blue
+        B. red
+        C. green
+        D. yellow`,
+        category: category3,
+        correctAnswer: 'C',
+        score: 300,
+    },
+    {
+        text: `what is the color of the sky?
+        A. blue
+        B. red
+        C. green
+        D. yellow`,
+        category: category4,
+        correctAnswer: 'A',
+        score: 200,
+    },
+    {
+        text: `What movie has the most won and nominated Academy Awards?
+        A. Harry Potter and the Sorcerer's Stone
+        B. Avatar
+        C. Titanic
+        D. The Lord of the Rings: The Fellowship of the Ring`,
+        category: category5,
+        correctAnswer: 'C',
+        score: 100,
+    }],
+    level200: [  {
+        text: `What year was Terminator 2: Judgement Day released?
+        A. 1991
+        B. 1993
+        C. 1995
+        D. 1997`, 
+        category: category1,
+        correctAnswer: 'A',
+        score: 500,
+    },
+    {
+        text: `what is the color of the sky?
+        A. blue
+        B. red
+        C. green
+        D. yellow`,
+        category: category2,
+        correctAnswer: 'A',
+        score: 400,
+    },
+    {
+        text: `what is the color of the sky?
+        A. blue
+        B. red
+        C. green
+        D. yellow`,
+        category: category3,
+        correctAnswer: 'C',
+        score: 300,
+    },
+    {
+        text: `what is the color of the sky?
+        A. blue
+        B. red
+        C. green
+        D. yellow`,
+        category: category4,
+        correctAnswer: 'A',
+        score: 200,
+    },
+    {
+        text: `What year was Spider-Man: No Way Home released?
+        A. 2021
+        B. 2019
+        C. 2017
+        D. 2022`,
+        category: category5,
+        correctAnswer: 'C',
+        score: 100,
+    }],
+    level100: [  {
+        text: `Where was The Forest Moon of Endor shot?
+        A. Australia
+        B. Africa
+        C. California
+        D. Florida`, 
+        category: category1,
+        correctAnswer: 'A',
+        score: 500,
+    },
+    {
+        text: `what is the color of the sky?
+        A. blue
+        B. red
+        C. green
+        D. yellow`,
+        category: category2,
+        correctAnswer: 'A',
+        score: 400,
+    },
+    {
+        text: `what is the color of the sky?
+        A. blue
+        B. red
+        C. green
+        D. yellow`,
+        category: category3,
+        correctAnswer: 'C',
+        score: 300,
+    },
+    {
+        text: `what is the color of the sky?
+        A. blue
+        B. red
+        C. green
+        D. yellow`,
+        category: category4,
+        correctAnswer: 'A',
+        score: 200,
+    },
+    {
+        text: `Where was Anchorman: The Legend of Ron Burgundy filmed?
+        A. New Jersey
+        B. Japan
+        C. California
+        D. Italy`,
+        category: category5,
+        correctAnswer: 'C',
+        score: 100,
+    }],
+}
+
 function displayScore(){
     document.getElementById('score').innerHTML = 'Score:' + ' ' + userScore
 }
 
-function checkAnswer(userAnswer, correctAnswer){
+function checkAnswer(userAnswer, correctAnswer, checkScore){
     if(userAnswer === correctAnswer){
         alert('Well Done. Correct answer')
-        userScore = userScore + 100
+        userScore+= checkScore
         displayScore()
     }
     else{
@@ -26,241 +286,6 @@ function getUserAnswerFromQuestion(question) {
     return userAnswer
 }
 
-const levels = ['500', '400', '300', '200', '100']
-
-
-
-const questions = {
-    level500: [
-        {
-            text: `This movie depicts a jewish father hiding the horrors of the holocaust from his son by turning their time at concentration camps into a game.
-            A. Life is Beautiful
-            B. Schindler's list
-            C. The Pianist
-            D. The Boy in the Striped Pajamas`, 
-        category: category1,
-        correctAnswer: 'A',
-        },
-        {
-            text: `Who voiced Stuart Little in the 1999 family-film "Stuart Little?
-            A. Michael J. Fox
-            B. Mark Hamill
-            C. Robin Williams
-            D. Jim Carrey`,
-            category: category2,
-            correctAnswer: 'A',
-        },
-        {
-            text: `What movie won the 2003 best visual effects Academy Award?
-            A. Master and Commander: The Far Side of the World
-            B. Pirates of the Caribbean: The Curse of the Black Pearl
-            C. The Lord of the Rings: The Return of the King
-            D. King Kong`,
-            category: category3,
-            correctAnswer: 'C',
-        },
-        {
-            text: `What year was Terminator 2: Judgement Day released?
-            A. 1991
-            B. 1993
-            C. 1995
-            D. 1997`,
-            category: category4,
-            correctAnswer: 'A',
-        },
-        {
-            text: `Where was The Forest Moon of Endor shot?
-            A. Australia
-            B. Africa
-            C. California
-            D. Florida`,
-            category: category5,
-            correctAnswer: 'C',
-        }
-    ],
-    level400: [
-        {
-            text: `what dog barks?
-            A. dog
-            B. cat
-            C. bird
-            D. fish`, 
-            category: category1,
-            correctAnswer: 'A',
-        },
-        {
-            text: `what is the color of the sky?
-            A. blue
-            B. red
-            C. green
-            D. yellow`,
-            category: category2,
-            correctAnswer: 'A',
-        },
-        {
-            text: `what childrens game involves two safe spots?
-            A. ring around the rosie
-            B. tag
-            C. running bases
-            D. hide and seek`,
-            category: category3,
-            correctAnswer: 'C',
-        },
-        {
-            text: `what is the color of the sky?
-            A. blue
-            B. red
-            C. green
-            D. yellow`,
-            category: category4,
-            correctAnswer: 'A',
-        },
-        {
-            text: `what childrens game involves two safe spots?
-            A. ring around the rosie
-            B. tag
-            C. running bases
-            D. hide and seek`,
-            category: category5,
-            correctAnswer: 'C',
-        }
-    ],
-    level300: [  {
-        text: `what dog barks?
-        A. dog
-        B. cat
-        C. bird
-        D. fish`, 
-        category: category1,
-        correctAnswer: 'A',
-    },
-    {
-        text: `what is the color of the sky?
-        A. blue
-        B. red
-        C. green
-        D. yellow`,
-        category: category2,
-        correctAnswer: 'A',
-    },
-    {
-        text: `what childrens game involves two safe spots?
-        A. ring around the rosie
-        B. tag
-        C. running bases
-        D. hide and seek`,
-        category: category3,
-        correctAnswer: 'C',
-    },
-    {
-        text: `what is the color of the sky?
-        A. blue
-        B. red
-        C. green
-        D. yellow`,
-        category: category4,
-        correctAnswer: 'A',
-    },
-    {
-        text: `what childrens game involves two safe spots?
-        A. ring around the rosie
-        B. tag
-        C. running bases
-        D. hide and seek`,
-        category: category5,
-        correctAnswer: 'C',
-    }],
-    level200: [  {
-        text: `what dog barks?
-        A. dog
-        B. cat
-        C. bird
-        D. fish`, 
-        category: category1,
-        correctAnswer: 'A',
-    },
-    {
-        text: `what is the color of the sky?
-        A. blue
-        B. red
-        C. green
-        D. yellow`,
-        category: category2,
-        correctAnswer: 'A',
-    },
-    {
-        text: `what childrens game involves two safe spots?
-        A. ring around the rosie
-        B. tag
-        C. running bases
-        D. hide and seek`,
-        category: category3,
-        correctAnswer: 'C',
-    },
-    {
-        text: `what is the color of the sky?
-        A. blue
-        B. red
-        C. green
-        D. yellow`,
-        category: category4,
-        correctAnswer: 'A',
-    },
-    {
-        text: `what childrens game involves two safe spots?
-        A. ring around the rosie
-        B. tag
-        C. running bases
-        D. hide and seek`,
-        category: category5,
-        correctAnswer: 'C',
-    }],
-    level100: [  {
-        text: `What does HTML stand for?
-        A. Hyper Text Markup Language
-        B. Hyperlinks and Text Markup Language
-        C. Home Tool Markup Language`, 
-        category: category1,
-        correctAnswer: 'A',
-    },
-    {
-        text: `what is the color of the sky?
-        A. blue
-        B. red
-        C. green
-        D. yellow`,
-        category: category2,
-        correctAnswer: 'A',
-    },
-    {
-        text: `what childrens game involves two safe spots?
-        A. ring around the rosie
-        B. tag
-        C. running bases
-        D. hide and seek`,
-        category: category3,
-        correctAnswer: 'C',
-    },
-    {
-        text: `what is the color of the sky?
-        A. blue
-        B. red
-        C. green
-        D. yellow`,
-        category: category4,
-        correctAnswer: 'A',
-    },
-    {
-        text: `what childrens game involves two safe spots?
-        A. ring around the rosie
-        B. tag
-        C. running bases
-        D. hide and seek`,
-        category: category5,
-        correctAnswer: 'C',
-    }],
-}
-
 function createBoard() {
     const level500 = questions.level500
     for (let i = 0 ; i < level500.length ; i++){    
@@ -270,7 +295,7 @@ function createBoard() {
                 const userAnswer = getUserAnswerFromQuestion(question.text)
                 //return to this later to account for null//
                 if (userAnswer !== null){
-                    checkAnswer(userAnswer, question.correctAnswer)
+                    checkAnswer(userAnswer, question.correctAnswer, question.score)
                 }
                 console.log(userAnswer)
                 td.style.backgroundColor = 'black'
@@ -287,7 +312,7 @@ function createBoard() {
             td.addEventListener('click', function(){               
                 const userAnswer = getUserAnswerFromQuestion(question.text)
                 if (userAnswer !== null){
-                    checkAnswer(userAnswer, question.correctAnswer)
+                    checkAnswer(userAnswer, question.correctAnswer, question.score)
                 }
                 console.log(userAnswer)
                 td.style.backgroundColor = 'black'
@@ -304,7 +329,7 @@ function createBoard() {
             td.addEventListener('click', function(){
                 const userAnswer = getUserAnswerFromQuestion(question.text)
                 if (userAnswer !== null){
-                    checkAnswer(userAnswer, question.correctAnswer)
+                    checkAnswer(userAnswer, question.correctAnswer, question.score)
                 }
                 console.log(userAnswer)
                 td.style.backgroundColor = 'black'
@@ -321,7 +346,7 @@ function createBoard() {
             td.addEventListener('click', function(){
                 const userAnswer = getUserAnswerFromQuestion(question.text)
                 if (userAnswer !== null){
-                    checkAnswer(userAnswer, question.correctAnswer)
+                    checkAnswer(userAnswer, question.correctAnswer, question.score)
                 }
                 console.log(userAnswer)
                 td.style.backgroundColor = 'black'
@@ -338,7 +363,7 @@ function createBoard() {
             td.addEventListener('click', function(){
                 const userAnswer = getUserAnswerFromQuestion(question.text)
                 if (userAnswer !== null){
-                    checkAnswer(userAnswer, question.correctAnswer)
+                    checkAnswer(userAnswer, question.correctAnswer, question.score)
                 }
                 console.log(userAnswer)
                 td.style.backgroundColor = 'black'
