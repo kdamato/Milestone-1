35721e2d2f03a5144f8a8346570e5c3add84f573
Kyle DAmato <kyledamato4@gmail.com>
2022-06-17 19:02:25 -0400

    completed horror jeopardy game

--- horror-jeopardy.js
+++ horror-jeopardy.js
@@ -9,226 +9,231 @@ var userScore = 0;
 const levels = ['500', '400', '300', '200', '100']
 
 const questions = {
+    //plot//
     level500: [
         {
-        text: `This movie depicts a jewish father hiding the horrors of the holocaust from his son by turning their time at concentration camps into a game.
-        A. Life is Beautiful
-        B. Schindler's list
-        C. The Pianist
-        D. The Boy in the Striped Pajamas`, 
+        text: `This supernatural thriller depicts a baby-sitter hired to take care of an 8-year old boy to find that it is just a life size doll. Or is it?...
+        A. Puppet Master
+        B. Chucky
+        C. The Boy
+        D. Annabelle`, 
         category: category1,
-        correctAnswer: 'A',
+        correctAnswer: 'C',
         score: 500,
         },
         {
-            text: `This movie is about a man who learns the men in his family can travel beginning on their 21st birthday.
-            A. About time
-            B. In time
-            C. The Adam Project
-            D. The Tomorrow War`,
+            text: `This 2019 horror-thriller is a late sequel to Stephen King's 1980, "The Shining"
+            A. Doctor Sleep
+            B. It
+            C. The Stand
+            D. Pet Sematary`,
             category: category2,
             correctAnswer: 'A',
             score: 400,
         },
         {
-            text: `A christmas rom-com about a father meeting his rich, future son-in-law for the first time. Little does the father know, his daughters fiancee is his biggest nightmare and he does not approve.
-            A. Four Christmases
-            B. Just Friends
-            C. Why Him?
-            D. Holidate`,
+            text: `This 2007 horror-thriller stars Thomas Jane with his young son after a powerful storm hits their home in Maine. This movies well known for its shocking twist-ending.
+            A. Twister
+            B. The Mist
+            C. Cloverfield
+            D. Crawl`,
             category: category3,
-            correctAnswer: 'C',
+            correctAnswer: 'B',
             score: 300,
         },
         {
-            text: `Not your typical superhero movie of a red suited vigilante?
-            A. Deadpool
-            B. Green Lantern
-            C. Thor
-            D. Spider-Man`,
+            text: `This action-horror film shows a former U-N investigator portrayed by Brad Pitt as he tries to find a cure for a lethal virus.
+            A. World War Z
+            B. Resident Evil
+            C. ZombieLand
+            D. Dawn of The Dead`,
             category: category4,
             correctAnswer: 'A',
             score: 200,
         },
         {
-            text: `What childrens movie stars the greek demi-god, the son of Zeus?
-            A. The Lion King
-            B. Toy Story
-            C. A Bug's Life
-            D. Hercules`,
+            text: `Dont say his name three times in the mirror! This supernatural, hook handed killer will unleash a terrifying wave of violence in the 1992 film "_______"
+            A. Sleepwalkers
+            B. The Ring
+            C. The Grudge
+            D. Candyman`,
             category: category5,
             correctAnswer: 'D',
             score: 100,
         }
     ],
+    //actors//
     level400: [
         {
-            text: `Who voiced Stuart Little in the 1999 family-film "Stuart Little?
-            A. Michael J. Fox
-            B. Mark Hamill
-            C. Robin Williams
-            D. Jim Carrey`,
+            text: `Which actress has played in both "The Grudge" and in "Insidious"?
+            A. Lin Shaye
+            B. Andrea Riseborough
+            C. Zoe Fish
+            D. Tara Westwood`,
             category: category1,
             correctAnswer: 'A',
             score: 500,
         },
         {
-            text: `Who played Mrs. Doubtfire in the movie "Mrs. Doubtfire"?
-            A. Robin Williams
-            B. Martin Lawrence
-            C. Will Ferrell
-            D. Eddie Murphy`,
+            text: `Who played Lt Ripley in the 1986 film, "Aliens"?
+            A. Colette Hiller
+            B. Jenette Goldstein
+            C. Carrie Henn
+            D. Sigourney Weaver`,
             category: category2,
-            correctAnswer: 'A',
+            correctAnswer: 'D',
             score: 400,
         },
         {
-            text: `Who plays Rasputia in Norbit?
-            A. Robin Williams
-            B. Martin Lawrence
-            C. Eddie Murphy
-            D. Will Ferrell`,
+            text: `Who plays the main protagonist in the 1978 "Halloween" Film?
+            A. Donald Pleasence
+            B. Nancy Kyes
+            C. Jamie Lee Curtis
+            D. Tony Moran`,
             category: category3,
             correctAnswer: 'C',
             score: 300,
         },
         {
-            text: `Which voices Darth Vader?
-            A. James Earl Jones
-            B. Hayden Christensen
-            C. David Prowse
-            D. Spencer Wilding`,
+            text: `Which plays Jason in the "Friday the 13th" franchise?
+            A. Rob Englund
+            B. Arie Lehman
+            C. Derek Mears
+            D. Walt Gorney`,
             category: category4,
-            correctAnswer: 'A',
+            correctAnswer: 'B',
             score: 200,
         },
         {
-            text: `Who plays Jack Sparrow in the Pirates of the Carribean franchise?
-            A. Matthew McConaughey
-            B. Hugh Jackman
+            text: `The 1984 "Nightmare on Elm Street" was who's first acting debut?
+            A. Robert Englund
+            B. Heather Langenkamp
             C. Johnny Depp
-            D. Jim Carrey`,
+            D. Amanda Wyss`,
             category: category5,
             correctAnswer: 'C',
             score: 100,
         }
     ],
+    //awards//
     level300: [  {
-        text: `What movie won the 2003 best visual effects Academy Award?
-            A. Master and Commander: The Far Side of the World
-            B. Pirates of the Caribbean: The Curse of the Black Pearl
-            C. The Lord of the Rings: The Return of the King
-            D. King Kong`,
+        text: `This record breaking psychological horror was the first to ever win Best Picture in 1991.
+            A. Silent Hill
+            B. Silence of the Lambs
+            C. The grudge
+            D. The Ring`,
         category: category1,
-        correctAnswer: 'C',
+        correctAnswer: 'B',
         score: 500,
     },
     {
-        text: `What film won the best cinematography award in 2010?
-        A. Avatar
-        B. Shuttered Island
-        C. Inception
-        D. Clash of the Titans`,
+        text: `This M. Night Shyamalan horror starring Haley Joel Osment earned 6 oscar nominations?
+        A. The Sixth Sense
+        B. The Shining
+        C. Split
+        D. Devil`,
         category: category2,
         correctAnswer: 'A',
         score: 400,
     },
     {
-        text: `What film won best actor in a leading role in 1995?
-        A. Billy Madison
-        B. Batman Forever
-        C. Forrest Gump
-        D. Seven`,
+        text: `What horror film won best screen-play in 2017?
+        A. It
+        B. In the Deep
+        C. Get Out
+        D. Happy Death Day`,
         category: category3,
         correctAnswer: 'C',
         score: 300,
     },
     {
-        text: `Who won best actor in a leading role in the 2015 film, "The Revenant"?
-        A. Leonardo DiCaprio
-        B. Tom Hardy
-        C. Will Poulter
-        D. Domhnall Gleeson`,
+        text: `This Steven Spielberg movie won Best Sound, Best Film Editing and Best Music: Original Dramatic Score in 1976.
+        A. The Twilight Zone
+        B. Jaws
+        C. Duel
+        D. Jurrasic Park`,
         category: category4,
-        correctAnswer: 'A',
+        correctAnswer: 'B',
         score: 200,
     },
     {
-        text: `What movie has the most won and nominated Academy Awards?
-        A. Harry Potter and the Sorcerer's Stone
-        B. Avatar
-        C. Titanic
-        D. The Lord of the Rings: The Fellowship of the Ring`,
+        text: `What 1973 horror film was nominated for 10 awards?
+        A. The Wicker Man
+        B. The Crazies
+        C. The Exorcist
+        D. The Hanging Woman`,
         category: category5,
         correctAnswer: 'C',
         score: 100,
     }],
+    //years//
     level200: [  {
-        text: `What year was Terminator 2: Judgement Day released?
-        A. 1991
-        B. 1993
-        C. 1995
-        D. 1997`, 
+        text: `What year was the exorcist released?
+        A. 1973
+        B. 1981
+        C. 1989
+        D. 1979`, 
         category: category1,
         correctAnswer: 'A',
         score: 500,
     },
     {
-        text: `What year was "James Bond: You Only Live Twice" released?
-        A. 1967
-        B. 1965
-        C. 1969
-        D. 1971`,
+        text: `What year was "Us" released?
+        A. 2019
+        B. 2020
+        C. 2018
+        D. 2017`,
         category: category2,
         correctAnswer: 'A',
         score: 400,
     },
     {
-        text: `What year was "Big Fat Liar," starring Frankie Muniz, released?
-        A. 2004
+        text: `What year was "Insidious" released?
+        A. 2008
         B. 2005
-        C. 2002
-        D. 1999`,
+        C. 2010
+        D. 2015`,
         category: category3,
         correctAnswer: 'C',
         score: 300,
     },
     {
-        text: `What year was Disney's "Moana" released?
-        A. 2016
+        text: `What year was "Unborn" released?
+        A. 2007
         B. 2014
-        C. 2018
-        D. 2022`,
+        C. 2009
+        D. 2005`,
         category: category4,
-        correctAnswer: 'A',
+        correctAnswer: 'C',
         score: 200,
     },
     {
-        text: `What year was Spider-Man: No Way Home released?
-        A. 2017
-        B. 2019
-        C. 2021
-        D. 2022`,
+        text: `What year was "Children of the Corn" released?
+        A. 2010
+        B. 1999
+        C. 2005
+        D. 1984`,
         category: category5,
-        correctAnswer: 'C',
+        correctAnswer: 'D',
         score: 100,
     }],
+    //location//
     level100: [  {
-        text: `Where was The Forest Moon of Endor shot?
-        A. Australia
-        B. Africa
+        text: `Where was "The Overlook Hotel" filmed?
+        A. Russia
+        B. Colorado
         C. California
-        D. Florida`, 
+        D. Maine`, 
         category: category1,
-        correctAnswer: 'C',
+        correctAnswer: 'B',
         score: 500,
     },
     {
-        text: `Where was the Mummy filmed?
-        A. Morocco and the United Kingdom
-        B. Egypt and the United States
-        C. Australia and the United Kingdom
-        D. Morocco and the United States`,
+        text: `Where was the scene in the 2001 horror "Hannibal" in which Hannibal Lecter pretends to be Doctor Fell before his most gruesome kill shot?
+        A. Italy
+        B. France
+        C. Rome
+        D. England`,
         category: category2,
         correctAnswer: 'A',
         score: 400,
@@ -244,21 +249,21 @@ const questions = {
         score: 300,
     },
     {
-        text: `Where was the cave of Dragon-Glass in Game of Thrones shot?
-        A. Iceland
-        B. Greenland
-        C. Germany
-        D. Australia`,
+        text: `Where is the Crystal Lake Diner, used for scenes in the 1980 film "Friday the 13th" located?
+        A. New Jersey
+        B. Maine
+        C. California
+        D. Texas`,
         category: category4,
         correctAnswer: 'A',
         score: 200,
     },
     {
-        text: `Where was Anchorman: The Legend of Ron Burgundy filmed?
-        A. New Jersey
-        B. Japan
-        C. California
-        D. Italy`,
+        text: `Where were the shots in the library of the movie "It" located?
+        A. United States
+        B. Europe
+        C. Canada
+        D. Mexico`,
         category: category5,
         correctAnswer: 'C',
         score: 100,
